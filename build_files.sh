bf89df7db2cabff1db495bbe588ddd11a5f99ddc
trulysaroj <sarojt7475@gmail.com>
2024-04-27 16:00:49 +0545

    venv enable

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,14 @@
 
-python3.10 -m pip install -r requirements.txt 
+# python3.10 -m pip install -r requirements.txt 
+# python3.10 manage.py collectstatic --noinput
+
+
+# Create and activate a virtual environment
+python3.10 -m venv env
+source env/Scripts/activate
+
+# Install Python dependencies
+python3.10 -m pip install -r requirements.txt
+
+# Collect static files
 python3.10 manage.py collectstatic --noinput
--- config/settings.py
+++ config/settings.py
@@ -158,5 +158,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
